69ad987be04c01095fc6b5a74381da0609c9de3d
Trịnh Xuân Minh <trinhxuanminh2000@gmail.com>
2022-08-16 15:37:32 +0700

    Initial Commit

---
+++ SelfieDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfieDemo
+//
+//  Created by Trịnh Xuân Minh on 16/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
